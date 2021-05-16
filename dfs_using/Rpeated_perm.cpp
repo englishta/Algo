62ed5c0d5abecaf3b7ee160f4a93874451c62f1d
englishta <1999ju373@gmail.com>
2021-05-16 15:20:11 +0900

    commit

--- dfs_using/Rpeated_perm.cpp
+++ dfs_using/Rpeated_perm.cpp
@@ -4,26 +4,20 @@
 using namespace std;
 using ll = long long;
 
-void dfs(ll i, string s, ll start, ll end, ll size, vector<string>& buf){
-	if(i == size){
-        buf.push_back(s);
-	}else{
-		for(ll j = start; j<=end; ++j){
-			dfs(i+1, s+char('0'+j), start, end, size, buf);
-		}
-	}
+void dfs(ll i, string s, string Elem, ll size, vector<string>& buf){
+	if(i == size) buf.push_back(s);
+	else for(auto e : Elem) dfs(i+1, s+e, Elem, size, buf);
 }
-vector<string> Repeated_Perm(ll start, ll end, ll size){
-// [start, end]からsize個選ぶ重複組み合わせ
-// o((end-start+1)^size)
+vector<string> Repeated_Perm(string Elem, ll size){
     vector<string> buf;
-    dfs(0, "", start, end, size, buf);
+    dfs(0, "", Elem, size, buf);
     return buf;
 }
 
+// 文字列Elemの要素からsize個選ぶ重複組み合わせ
+// o(|Elem|^size)
 int main(){
-    // (1, 2, 3, 4, 5)->3つ選ぶ
-    vector<string> x = Repeated_Perm(1, 5, 3);
+    auto x = Repeated_Perm("abcd", 3);
     for(auto e : x){
         cout << e << endl;
     }
