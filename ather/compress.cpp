ec2b5cfaa3426ced0114d1547da002324cf1a0a5
englishta <1999ju373@gmail.com>
2021-08-09 09:33:09 +0900

    commit

--- ather/compress.cpp
+++ ather/compress.cpp
@@ -1,26 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
 template<class T>
-vector<T> compress(vector<T> &X){
-    vector<T> Uni = X;
-    sort(all(Uni));
-    UNIQUE(Uni);
-    for(ll i=0; i<(ll)X.size(); i++) X[i]=lb(Uni, X[i]);
-    return X; 
+vector<T> compress(vector<T> X){
+    ll n = X.size();
+    vector<T> U = X, ans(n);
+    sort(U.begin(), U.end());
+    U.erase(unique(U.begin(), U.end()), U.end());
+    for(ll i=0; i<n; i++){
+        ans[i]=distance((U).begin(), lower_bound(U.begin(), U.end(), X[i]));
+    }
+    return ans;
 }
 
 vector<ll> compress(vector<ll> arr){
-  ll n=arr.size();
-  map<ll,ll> m;
-  for(int i=0;i<n;i++){
-    m[arr[i]]=0;
-  }
-  ll s=0;
-  for(auto& i:m){
-    i.second=s++;
-  }
-  vector<ll> ans;
-  for(int i=0;i<n;i++){
-    ans.push_back(m[arr[i]]);
-  }
-  return ans;
+    ll n=arr.size();
+    map<ll,ll> m;
+    for(int i=0;i<n;i++){
+        m[arr[i]]=0;
+    }
+    ll s=0;
+    for(auto& i:m){
+        i.second=s++;
+    }
+    vector<ll> ans;
+    for(int i=0;i<n;i++){
+        ans.push_back(m[arr[i]]);
+    }
+    return ans;
 }
 
