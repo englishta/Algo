27293ad4008fbe43d68213c90e256f7aecc119bd
oliver <1999ju373@gmail.com>
2021-09-15 09:09:07 +0900

    commit

--- Divisor/isprime.cpp
+++ Divisor/isprime.cpp
@@ -1,7 +1,23 @@
-bool isprime(int N) {
+#include <iostream>
+using namespace std;
+
+// 素数を判定する関数
+bool isprime(long long N) {
     if (N < 2) return false;
-    for (int i = 2; i < N; ++i) {
+    for (long long i = 2; i * i <= N; ++i) {
         if (N % i == 0) return false;
     }
     return true;
+}
+
+int main() {
+    // 入力
+    long long N;
+    cin >> N;
+
+    // 判定
+    if (isprime(N))
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
 }
