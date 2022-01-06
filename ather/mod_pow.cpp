53c9b733e7a981ee4e13c8d15e0f62068add89cd
oliver <1999ju373@gmail.com>
2022-01-06 17:04:01 +0900

    update

---
+++ ather/mod_pow.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// a^{n} mod ‚ðŒvŽZ‚·‚é
+long long modpow(long long a, long long n, long long mod) {
+    long long res = 1;
+    while (n > 0) {
+        if (n & 1) res = res * a % mod;
+        a = a * a % mod;
+        n >>= 1;
+    }
+    return res;
+}
+
+// a^{-1} mod ‚ðŒvŽZ‚·‚é
+long long modinv(long long a, long long mod) {
+    return modpow(a, mod - 2, mod);
+}
+
+
+
+
